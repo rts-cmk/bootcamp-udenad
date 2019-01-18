931798cf5a9527b6cf385a0a19445f81dcbae191
Ady Moussa <adymoussa.edu@gmail.com>
2019-01-18 10:42:11 +0100

    Test - GitHub Pull

--- eks_01a/js/index.js
+++ eks_01a/js/index.js
@@ -1,4 +1,6 @@
 
+// GitHub Pull test: Hej :)
+
 // "DOMContentLoaded" event'en sørger for, at Javascriptet først kører når hele HTML dokumentet er indlæst
 document.addEventListener ("DOMContentLoaded", () => {
 
