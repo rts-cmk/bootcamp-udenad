9d1231891add41fa4f847a69cb169cadb9c27619
clurts <clu@rts.dk>
2020-01-17 20:36:48 +0100

    Add files via upload

---
+++ eks_01b_DOMogEvents/js/index.js
@@ -0,0 +1,6 @@
+// "DOMContentLoaded" event'en sørger for, at Javascriptet først kører når hele HTML dokumentet er indlæst
+document.addEventListener("DOMContentLoaded", () => {
+
+	// skriv din kode her...
+
+}); // Afslutter: DOMContentLoaded
