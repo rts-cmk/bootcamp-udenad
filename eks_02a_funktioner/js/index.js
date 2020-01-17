2fd1d1f1436021fb194bcf53ff39e9f1e9ae2ec1
clurts <clu@rts.dk>
2020-01-17 20:39:03 +0100

    Add files via upload

--- eks_02a_funktioner/js/index.js
+++ eks_02a_funktioner/js/index.js
@@ -1,13 +1,6 @@
-
 // "DOMContentLoaded" event'en sørger for, at Javascriptet først kører når hele HTML dokumentet er indlæst
-document.addEventListener ("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => {
 
-	// Kalder en funktion  (husk de runde parenteser)
-	hils ();
-
-	// Definerer en funktion, som hverken tager imod argumenter (parametre) eller returnerer noget
-	function hils () {
-		console.log ("Hej med dig");
-	}
+	// skriv din kode her...
 
 }); // Afslutter: DOMContentLoaded
